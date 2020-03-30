dc0b9b811d815c364242860e619cac86757c1d46
Miguel Cobas <mcobas@dealergeek.com>
2020-03-29 22:26:58 -0600

    Se creo archivo de configuración para que sea mas facil de leer y modificar, adicionalmente se crearon las variables para entornos de produccion

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 
 const mongoose = require('mongoose')
 const app = require('./app')
-
-
-const port = process.env.PORT || 3001
+const config = require('./config')
 
 
 
@@ -17,13 +15,13 @@ const port = process.env.PORT || 3001
 
 
 //el puerto por defecto es 27017
-mongoose.connect('mongodb://localhost:27017/shop', (err, res)=>{
+mongoose.connect(config.db, (err, res)=>{
     if(err) {
         return console.log(`Error al conectar a la base de datos: ${err}`)
     }
     console.log('conexion a la base de datos establecida')
-    app.listen(port, () =>{
-        console.log(`API REST  corriendo en http://localhost:${port}`)
+    app.listen(config.port, () =>{
+        console.log(`API REST  corriendo en http://localhost:${config.port}`)
     })
 })
 
